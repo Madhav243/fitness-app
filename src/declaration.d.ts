c0aa199a0ffabb35a9e8fc1ec6f0dd4574f73e55
Madhav Verma <56861607+Madhav243@users.noreply.github.com>
2025-04-16 20:20:10 +0530

    Update declaration.d.ts

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,2 +1,9 @@
 declare module '*.jpg';
 declare module '*.png';
+
+declare module 'react-dom/client' {
+  import * as ReactDOM from 'react-dom';
+
+  export function createRoot(container: Element | DocumentFragment): ReactDOM.Root;
+  export function hydrateRoot(container: Element | DocumentFragment, initialChildren: React.ReactNode): ReactDOM.HydrateRoot;
+}
